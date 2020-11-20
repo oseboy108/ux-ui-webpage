e3139a414cad48ec0f265a2db2f859fe645f1a3a
ehiaguina francis <ehiaguinaosesojie@gmail.com>
2020-11-20 16:20:49 +0100

    innit

---
+++ index.js
@@ -0,0 +1,37 @@
+ //My first javascript code
+console.log('Hello world');
+console.error('this is an error')
+console.warn('this is a warning')
+
+
+ //Let, const
+let age=30;
+age=31
+
+const score=45;
+
+
+
+
+ console.log(age);
+ 
+ console.log(score);
+
+ //strings, Numbers,Boolean, null, undefined
+
+ const name='john';
+ const age=30
+ const rating=4.5;
+ const x=null;
+ const y=undefined;
+ let z;
+
+ console.log(typeofx)
+
+
+ const fruits= ['apples','oranges','pears','10','true']
+
+ console.log(fruits)
+
+
+
